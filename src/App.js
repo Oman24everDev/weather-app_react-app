4383e64a31f3a638d222c75aaaf1cfa825ec35ce
Oman24everDev <oman24ever@gmail.com>
2023-02-21 11:58:37 +0800

    create components folder & added search component with import AsyncPaginate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import Search from  "./components/search/search"
+import "./style.css"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, Sample!</h1>
+    <div className="container">
+      <Search />
 
     </div>
   );
---
+++ src/components/search/search.js
@@ -0,0 +1,10 @@
+import { AsyncPaginate } from "react-select-async-paginate";
+// this AsyncPaginate is install using npm i react-select-async-paginate --force
+
+const Search = () => {
+    return (
+        <AsyncPaginate placeholder="Search for City" />
+    )
+}
+
+export default Search;
