3b5010d949d18b68300636ef550a4263c0bbbc05
Oman24everDev <oman24ever@gmail.com>
2023-02-21 17:31:25 +0800

    create property of loadingOptions & integrates API, set into const variable & import the URL & Opt

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {  // rename the options into geoApiOptions then export
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '584c938872msh5a0e1cf02a5269fp197d03jsna83433d26996',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+// created this new const variable & export the URL
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,11 +1,24 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 // this AsyncPaginate is install using npm i react-select-async-paginate --force
+import { GEO_API_URL, geoApiOptions } from "../../api";
+
 
             // this onSearchChange is function to pass the event into App.js file
 const Search = ({onSearchChange}) => {
 
     const [search, setSearch] = useState(null);
+
+                // this inputValue is the value where we type in the input, & use that value to pass 
+                // it to fetch method to URL and get the DATA
+    const loadingOpt = (inputValue) => {
+        return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions)
+        // this line is to filter out the small places & limit to 1M population to give much better search 
+        // the namePrefix is to get what is in the params inputValue
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+    }
     
     // the searchData here is to retrieve the data that input in AsyncPaginate component
     const handleOnChange = (searchData) => {
@@ -19,6 +32,8 @@ const Search = ({onSearchChange}) => {
         debounceTimeout={600}
         value={search}
         onChange={handleOnChange}
+        loadOptions={loadingOpt}    // this property w/ method of loadingOpt is for every type on searchbar 
+                                    // it will fetch & load the data from API through Async Request
         />
     )
 }
