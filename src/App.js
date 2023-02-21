62e89631b3331468cb06a8d7ffcf48e8854236a8
Oman24everDev <oman24ever@gmail.com>
2023-02-21 12:13:43 +0800

    created variable handleOnChange for search bar & import useState

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import Search from  "./components/search/search"
 import "./style.css"
 
 function App() {
+
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  }
+
   return (
     <div className="container">
-      <Search />
+      <Search onSearchChange={handleOnSearchChange} />
 
     </div>
   );
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 // this AsyncPaginate is install using npm i react-select-async-paginate --force
 
-const Search = () => {
+const Search = ({onSearchChange}) => {
+
+    const [search, setSearch] = useState(null);
+    
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+    }
+
     return (
-        <AsyncPaginate placeholder="Search for City" />
+        <AsyncPaginate
+        placeholder="Search for City"
+        debounceTimeout={600}
+        value={search}
+        onChange={handleOnChange}
+        />
     )
 }
 
