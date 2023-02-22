37b2f1e882130c0529a974242a35ea540ec806dc
Oman24everDev <oman24ever@gmail.com>
2023-02-22 14:59:35 +0800

    customize the bottom-section

--- src/components/current-weather/currentWeather.js
+++ src/components/current-weather/currentWeather.js
@@ -10,6 +10,30 @@ const CurrentWeather = () => {
         </div>
         <img alt="weather" className="weather-icon" src="weather-icons/01d.png"/>
       </div>
+      <div className="bottom-section">
+        <p className="temperature">22°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="label detail">Label Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="label">Feels like</span>
+            <span className="value">18°C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="label">Wind</span>
+            <span className="value">3 m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span className="label">Humidity</span>
+            <span className="value">14 %</span>
+          </div>
+          <div className="parameter-row">
+            <span className="label">Pressure</span>
+            <span className="value">16 hPa</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
