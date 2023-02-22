f1bfb7129d9d19b9641aa74b8f60469d11ad0b4d
Oman24everDev <oman24ever@gmail.com>
2023-02-22 19:00:39 +0800

    added comment & implement the loop img with icons

--- src/components/forecast-weather/forecastWeather.js
+++ src/components/forecast-weather/forecastWeather.js
@@ -16,13 +16,18 @@ const ForecastWeather = ({ data }) => {
 
       {/* the property allowZeroExpanded is to allow all the accordion to be close otherwise one of the accordion needs to be open up*/}
       <Accordion allowZeroExpanded>
-        {/* this line if for map on object 'list' and get only 7 results using splice */}
+
+        {/* this line is a loop, if for map on object 'list' and get only 7 results using splice */}
+        {/* the params item is for accessing the data from objects instead of data.sample.sample */}
+        {/* the params index is for the key to Accordion to loop */}
         {data.list.splice(0, 7).map((item, index) => (
           <AccordionItem key={index}>
             <AccordionItemHeading>
-                <AccordionItemButton>
-                    Hello
-                </AccordionItemButton>
+              <AccordionItemButton>
+                <div className="daily-item">
+                  <img alt="weather" className="icon-small" src={`weather-icons/${item.weather[0].icon}.png`}/>
+                </div>
+              </AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel></AccordionItemPanel>
           </AccordionItem>
