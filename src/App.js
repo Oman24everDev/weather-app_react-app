395f9d110b54e850c82ea1679d2ec28017464210
Oman24everDev <oman24ever@gmail.com>
2023-02-22 16:01:45 +0800

    added the forecastFetch, Promise & created the var for response json & added a useState hooks

--- src/App.js
+++ src/App.js
@@ -2,20 +2,40 @@ import Search from "./components/search/search";
 import "./style.css";
 import CurrentWeather from "./components/current-weather/currentWeather";
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+import { useState } from "react";
 
 function App() {
+
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
   const handleOnSearchChange = (searchData) => {
     // console.log(searchData);
 
     // this line is the result from handleOnSearchChange,
     // to get the value in object from Geo API (lat & lon),
-    // then pass it to variable to template literal in currentWeatherFetch 
+    // then pass it to variable to template literal in currentWeatherFetch
     const [lat, lon] = searchData.value.split(" ");
 
     // this line is to fetch the openweather API lat & lon for 1st current weather API call
     const currentWeatherFetch = fetch(
       `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
     );
+
+    // this line is to fetch the 2nd api call from openweather API which is the forecast
+    // same API_URL the difference is the /forecast
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    );
+
+    // the passing of variable on array is ordered 1st is current 2nd is forecast
+    Promise.all([currentWeatherFetch, forecastFetch]).then(async (response) => {
+      // the json is to map on the response & pass to variable, the index 0 is for the 1st result
+      const weatherResponse = await response[0].json();
+      const forecastResponse = await response[1].json(); 
+    });
+
+    // !!! after fetch & create the Promise, next is the useState hooks
   };
 
   return (
--- src/api.js
+++ src/api.js
@@ -11,5 +11,6 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
 
 
 // this API is from the openweathermap.org, the key is from the account My API keys
+// this URL is for 1st api call for current weather, 2nd use same URL for forecast api call
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 export const WEATHER_API_KEY = "5e24674f787adfbfbfbfbf77066331be";
