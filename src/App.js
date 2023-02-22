7071a43bc8640695d88063c5f9908d1544f29620
Oman24everDev <oman24ever@gmail.com>
2023-02-22 15:28:35 +0800

    fetch the api from openweather & store it to api.js, create variable for fetch & split the value in object for lat & lon

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import Search from  "./components/search/search"
-import "./style.css"
+import Search from "./components/search/search";
+import "./style.css";
 import CurrentWeather from "./components/current-weather/currentWeather";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 
 function App() {
-
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
-  }
+    // console.log(searchData);
+
+    // this line is the result from handleOnSearchChange,
+    // to get the value in object from Geo API (lat & lon),
+    // then pass it to variable to template literal in currentWeatherFetch 
+    const [lat, lon] = searchData.value.split(" ");
+
+    // this line is to fetch the openweather API lat & lon for 1st current weather API call
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    );
+  };
 
   return (
     <div className="container">
-        {/* this handleOnSearchChange is to get the input from search bar then pass it to onSearchChange
+      {/* this handleOnSearchChange is to get the input from search bar then pass it to onSearchChange
             5. from the onSearchChange coming into search.js is passing the value to handleOnSearchChange then console.log to check result */}
       <Search onSearchChange={handleOnSearchChange} />
 
--- src/api.js
+++ src/api.js
@@ -8,3 +8,8 @@ export const geoApiOptions = {  // rename the options into geoApiOptions then ex
 
 // created this new const variable & export the URL
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+
+// this API is from the openweathermap.org, the key is from the account My API keys
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "5e24674f787adfbfbfbfbf77066331be";
