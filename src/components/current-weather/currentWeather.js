a42245030410e2eb15f34647864f2e07ad9d397f
Oman24everDev <oman24ever@gmail.com>
2023-02-22 14:32:38 +0800

    customize top-section

--- src/components/current-weather/currentWeather.js
+++ src/components/current-weather/currentWeather.js
@@ -1,15 +1,17 @@
-import "../current-weather/currentWeather-style.css"
+import "../current-weather/currentWeather-style.css";
 
 const CurrentWeather = () => {
-    return (
-        <div className="weather">
-            <div className="top-section">
-                <p className="city">Germany</p>
-                <p className="weather-desc">Rainy</p>
-            </div>
-            <img alt="weather" className="weather-icon" src="weather-icons/01d.png" />
+  return (
+    <div className="weather">
+      <div className="top-section">
+        <div>
+          <p className="city">Germany</p>
+          <p className="weather-desc">Rainy</p>
         </div>
-    );
-}
+        <img alt="weather" className="weather-icon" src="weather-icons/01d.png"/>
+      </div>
+    </div>
+  );
+};
 
 export default CurrentWeather;
